6501a7d93754cd879e9704f706976c6a7cdee44a
ReshmaSabareesh <reshmaedukat@gmail.com>
2022-03-10 23:07:56 +0530

    commit

--- project-client-master/src/pages/studentRegister/Validation.js
+++ project-client-master/src/pages/studentRegister/Validation.js
@@ -1,25 +1,25 @@
 function validation(values){ //values=useState fomvalue
     const errors = {}; //storing errors
-    const regex = /^[^\s@]+@[^s@]+\.[^\s@]{2,}+$/i ;
-    const regExpMobile = /^[+-]?\d*(?:[.,]\d*)?$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})+$/;
+    //const regex = /^[^\s@]+@[^s@]+\.[^\s@]{2,}+$/i ;
+    //const regExpMobile = /^[+-]?\d*(?:[.,]\d*)?$/;
+    //const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})+$/;
     if(!values.name){
         errors.name = "username is required";
     }
     if(!values.email){
         errors.email = "email is required";
     }
-    else if(!regex.test(values.email)){
-        errors.email = "email is invalid";
-    }
+    // else if(!regex.test(values.email)){
+    //     errors.email = "email is invalid";
+    // }
     if(!values.password){
         errors.password = "Password is required";
     }
-     else if(!passwordRegex.test(values.password)){
-        errors.password = "A minimum 8 characters password contains a combination of uppercase and lowercase letter and number are required.";
-    }
-    if(!regExpMobile.test(values.phone)){
-        errors.phone = "Enter valid phone no";
+    // else if(!passwordRegex.test(values.password)){
+       // errors.password = "A minimum 8 characters password contains a combination of uppercase and lowercase letter and number are required.";
+    //}
+    if(!values.phone){
+       errors.phone = "Enter valid phone no";
     }
     if(!values.place){
         errors.place = "Field is empty";
